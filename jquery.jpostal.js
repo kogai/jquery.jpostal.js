018b772aa7630a9c1dd45a586e6a5f8a3e0dae98
小飼慎一 <kogai0121@gmail.com>
2015-02-07 14:35:23 +0900

    require()を使えるように

--- jquery.jpostal.js
+++ jquery.jpostal.js
@@ -1,13 +1,26 @@
 /**
  * jquery.jpostal.js ver2.2
- * 
+ *
  * Copyright 2014, Aoki Makoto, Ninton G.K. http://www.ninton.co.jp
- * 
+ *
  * Released under the MIT license - http://en.wikipedia.org/wiki/MIT_License
- * 
+ *
  * Requirements
  * jquery.js
  */
+
+// Export the Underscore object for **Node.js**, with
+// backwards-compatibility for the old `require()` API. If we're in
+// the browser, add `_` as a global object.
+if (typeof exports !== 'undefined') {
+	if (typeof module !== 'undefined' && module.exports) {
+		exports = module.exports = JpostalDatabase;
+	}
+	exports.JpostalDatabase = JpostalDatabase;
+} else {
+	root.JpostalDatabase = JpostalDatabase;
+}
+
 function JpostalDatabase ( i_options ) {
 	this.address = [];	// database cache
 	this.map     = {
@@ -16,19 +29,19 @@ function JpostalDatabase ( i_options ) {
 		'http'  : '//jpostal.googlecode.com/svn/trunk/json/',
 		'https' : '//jpostal.googlecode.com/svn/trunk/json/',
 	};
-	
+
 	this.find = function ( i_postcode ) {
 		var	address = [];
-		
+
 		for ( var i = 0; i < this.address.length; ++i ) {
 			if ( this.address[i][0] == '_' + i_postcode ) {
 				address = this.address[i];
 			}
 		}
-		
+
 		return address;
 	};
-	
+
 	this.get = function ( i_postcode ) {
 		//	--------------------------------------------------
 		//	i_postcode	find()	find()	result
@@ -47,7 +60,7 @@ function JpostalDatabase ( i_options ) {
 		var defaults = ['', '', '', '', '', ''];
 		var	address;
 		var	head3;
-		
+
 		switch ( i_postcode.length ) {
 			case 3:
 			case 4:
@@ -57,7 +70,7 @@ function JpostalDatabase ( i_options ) {
 				address = this.find(head3);
 				address = $.extend( defaults, address );
 				break;
-				
+
 			case 7:
 				address = this.find(i_postcode);
 				if ( !address ) {
@@ -66,44 +79,44 @@ function JpostalDatabase ( i_options ) {
 				}
 				address = $.extend( defaults, address );
 				break;
-			
+
 			default:
 				address = defaults;
 				break;
 		}
-		
+
 		return address;
 	};
 
 	this.getUrl = function ( i_head3 ) {
 		var url = '';
-		
+
 		switch ( window.location.protocol ) {
 			case 'http:':
 				url = this.url['http'];
 				break;
-			
+
 			case 'https:':
 				url = this.url['https'];
 				break;
 		}
 		url = url + i_head3 + '.json';
-		
+
 		return url;
 	};
 
 	this.request = function ( i_postcode, i_callback ) {
 		var _this = this;
 		var head3 = i_postcode.substr(0, 3);
-		
+
 		if ( i_postcode.length <= 2 || this.getStatus(head3) != 'none' || head3.match(/[^0-9]/) ) {
 			return false;
 		}
 		this.setStatus(head3, 'waiting');
-		
+
 		var url = this.getUrl( head3 );
-		
-		var options = { 
+
+		var options = {
 			async         : false,
 			dataType      : 'jsonp',
 			jsonpCallback : 'jQuery_jpostal_callback',
@@ -119,12 +132,12 @@ function JpostalDatabase ( i_options ) {
 		$.ajax( options );
 		return true;
 	};
-	
+
 	this.save = function ( i_data ) {
 		for ( var i = 0; i < i_data.length; ++i ) {
 			var	rcd = i_data[i];
 			var postcode = rcd[0];
-			
+
 			if ( typeof this.map[postcode] == 'undefined' ) {
 				this.address.push( rcd );
 				this.map[postcode] = {state : 'complete', time : 0};
@@ -146,40 +159,40 @@ function JpostalDatabase ( i_options ) {
 		//	--------------------------------------------------
 		var st = '';
 		var	postcode = '_' + i_postcode;
-		
+
 		if ( typeof this.map[postcode] == 'undefined' ) {
 			// # 1
 			st = 'none';
-			
+
 		} else if ( 'complete' == this.map[postcode].state ) {
 			// # 2
 			st = 'complete';
-			
+
 		} else {
 			var t_ms = (new Date()).getTime() - this.map[postcode].time;
 			if ( t_ms < 5000 ) {
 				// # 3
 				st = 'waiting';
-			
+
 			} else {
 				// # 4
 				st = 'none';
 			}
 		}
-		
+
 		return st;
 	};
-	
+
 	this.setStatus = function ( i_postcode ) {
 		var	postcode = '_' + i_postcode;
-		
+
 		if ( typeof this.map[postcode] == 'undefined' ) {
 			this.map[postcode] = {
 				state : 'waiting',
 				time  : 0
 			};
 		}
-		
+
 		this.map[postcode].time = (new Date()).getTime();
 	};
 
@@ -191,38 +204,38 @@ function Jpostal ( i_JposDb ) {
 	this.options  = {};
 	this.postcode = '';
 	this.minLen   = 3;
-	
+
 	this.displayAddress = function () {
 		if ( this.postcode == '000info') {
 			this.address[2] += ' ' + this.getScriptSrc();
 		}
-		
+
 		console.log( this.address );
 		for ( var key in this.options.address ) {
 			var s = this.formatAddress( this.options.address[key], this.address );
 			$(key).val( s );
 		}
 	};
-	
+
 	this.formatAddress = function ( i_fmt, i_address ) {
 		var	s = i_fmt;
-		
+
 		s = s.replace( /%3|%p|%prefecture/, i_address[1] );
 		s = s.replace( /%4|%c|%city/      , i_address[2] );
 		s = s.replace( /%5|%t|%town/      , i_address[3] );
 		s = s.replace( /%6|%a|%address/   , i_address[4] );
 		s = s.replace( /%7|%n|%name/      , i_address[5] );
-		
+
 		return s;
 	};
 
 	this.getScriptSrc = function () {
 		var src = '';
-		
+
 		var	el_arr = document.getElementsByTagName('script');
 		for ( var i = 0; i < el_arr.length; ++i ) {
 			if ( 0 <= el_arr[i].src.search(/jquery.jpostal.js/) ) {
-				src = el_arr[i].src; 		
+				src = el_arr[i].src;
 			}
 		}
 
@@ -230,35 +243,35 @@ function Jpostal ( i_JposDb ) {
 	}
 
 	this.init = function ( i_options ) {
-		
+
 		if ( typeof i_options.postcode == 'undefined' ) {
 			throw new Error('postcode undefined');
 		}
 		if ( typeof i_options.address == 'undefined' ) {
 			throw new Error('address undefined');
 		}
-		
+
 		this.options.postcode = [];
 		if ( typeof i_options.postcode == 'string' ) {
 			this.options.postcode.push( i_options.postcode );
 		} else {
 			this.options.postcode = i_options.postcode;
 		}
-		
+
 		this.options.address = i_options.address;
-		
+
 		if ( typeof i_options.url != 'undefined' ) {
 			this.jposDb.url = i_options.url;
 		}
 	};
-	
+
 	this.main = function () {
 		this.scanPostcode();
 		if ( this.postcode.length < this.minLen ) {
-			// git hub issue #4: 郵便番号欄が0～2文字のとき、住所欄を空欄にせず、入力内容を維持してほしい 
+			// git hub issue #4: 郵便番号欄が0～2文字のとき、住所欄を空欄にせず、入力内容を維持してほしい
 			return ;
 		}
-		
+
 		var _this = this;
 		var f = this.jposDb.request( this.postcode, function () {
 			_this.callback();
@@ -267,7 +280,7 @@ function Jpostal ( i_JposDb ) {
 			this.callback();
 		}
 	};
-	
+
 	this.callback = function () {
 		this.address = this.jposDb.get( this.postcode );
 		this.displayAddress();
@@ -275,11 +288,11 @@ function Jpostal ( i_JposDb ) {
 
 	this.scanPostcode = function () {
 		var s = '';
-		
+
 		switch ( this.options.postcode.length ) {
 			case 0:
 				break;
-			
+
 			case 1:
 				//	github issue #8: 1つ目を空欄、2つ目を「001」としても、「001」として北海道札幌市を表示してしまう
 				//	----------------------------------------
@@ -309,7 +322,7 @@ function Jpostal ( i_JposDb ) {
 					s = '';
 				}
 				break;
-			
+
 			case 2:
 				//	github issue #8: 1つ目を空欄、2つ目を「001」としても、「001」として北海道札幌市を表示してしまう
 				//	----------------------------------------
@@ -326,7 +339,7 @@ function Jpostal ( i_JposDb ) {
 				if ( 0 <= s3.search(/^[0-9]{3}$/) ) {
 					if ( 0 <= s4.search(/^[0-9A-Za-z]{4}$/) ) {
 						// case 3
-						s = s3 + s4;					
+						s = s3 + s4;
 					} else {
 						// case 2
 						s = s3;
@@ -337,12 +350,12 @@ function Jpostal ( i_JposDb ) {
 				}
 				break;
 		}
-		
+
 		this.postcode = s;
-	};	
+	};
 }
 
-//	MEMO: For the following reason, JposDb was put on the global scope, not local scope. 
+//	MEMO: For the following reason, JposDb was put on the global scope, not local scope.
 //	---------------------------------------------------------------------
 // 	data file	callback			JposDb scope
 //	---------------------------------------------------------------------
@@ -359,11 +372,11 @@ function jQuery_jpostal_callback( i_data ) {
 	$.fn.jpostal = function( i_options ){
 		var Jpos = new Jpostal( JposDb );
 		Jpos.init( i_options );
-		
+
 		if ( typeof i_options.click == 'string' && i_options.click != '' ) {
 			$(i_options.click).bind('click', function(e) {
 				Jpos.main();
-			});			
+			});
 		} else {
 			for ( var i = 0; i < Jpos.options.postcode.length; ++i ) {
 				var selector = Jpos.options.postcode[i];
